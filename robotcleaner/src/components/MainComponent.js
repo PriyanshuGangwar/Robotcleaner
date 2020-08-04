bc1f5980f5427ce3a3fa68c3aa969443a5122165
Priyanshu Gangwar <priyanshugangwar02@gmail.com>
2020-08-04 22:25:07 +0530

    Final Logic

--- robotcleaner/src/components/MainComponent.js
+++ robotcleaner/src/components/MainComponent.js
@@ -1,96 +1,110 @@
-import React , {Component} from 'react';
-import { Navbar, NavbarBrand,Button} from 'reactstrap';
-import {robotclean} from './robot';
+import React, { Component } from "react";
+import { robotclean } from "./robot";
 
 function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      array: [],
+      rtop: 5,
+      rleft: 5,
+    };
   }
-class Main extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            array : [],
-            rtop : 5,
-            rleft : 5
-        };
+
+  componentDidMount() {
+    this.reset_array();
+  }
+
+  start() {
+    const finish = robotclean(this.state.array);
+    const animations = finish[0];
+    const visited = finish[1];
+    //console.log(animations.length, visited.length);
+    for (let i = 0; i < animations.length; i++) {
+      for (let j = 0; j < animations[i].length; j++) {
+        const arrayBars = document.getElementsByClassName("bar");
+        const robo = document.getElementsByClassName("robot");
+
+        const barOneIdx = j;
+        const robostyle = robo[0].style;
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const newleft = visited[i][1] * 40 + 5;
+        const newtop = visited[i][0] * 40 + 5;
+        setTimeout(() => {
+          var color = "blue";
+          if (animations[i][j] === 2) color = "green";
+          else if (animations[i][j] === 1) color = "rgb(106, 98, 145)";
+          else color = "black";
+          robostyle.top = `${newtop}px`;
+          robostyle.left = `${newleft}px`;
+          barOneStyle.backgroundColor = color;
+        }, i * 100);
+      }
+    }
+  }
+
+  reset_array() {
+    const row = [];
+    for (var i = 0; i < 300; i++) {
+      row.push(getRandom(0, 10));
     }
 
-    componentDidMount() {
-        this.reset_array();
-    }
-
-    start(){
-        const animations = robotclean(this.state.array);
-        console.log(animations);
-        for (let i = 0; i < animations.length; i++) {
-            
-            for (let j = 0; j < animations[i].length; j++) {
-                const arrayBars = document.getElementsByClassName('bar');
-                const robo = document.getElementsByClassName('robot');
-                
-                const barOneIdx = j; 
-                const robostyle = robo[0].style;
-                const barOneStyle = arrayBars[barOneIdx].style;
-                const newleft = ((j%20) * 40) + 5;
-                const newtop = ((j%15) * 40) + 5;
-                setTimeout(() => {
-                    var color = "blue";
-                    if(animations[i][j] === 2)
-                        color = "green";
-                    else if(animations[i][j] === 1)
-                        color =  "rgb(106, 98, 145)";
-                    else
-                        color  = "black";  
-                    robostyle.top = `${newtop}px`;
-                    robostyle.left = `${newleft}px`;
-                    barOneStyle.backgroundColor = color;
-                    },i*100);
-                    
-                    
-               }
-            }
-        }
-    
-
-    reset_array() {
-        const row = [];
-        for(var i = 0;i<300;i++){
-            row.push(getRandom(0,10));
-        }
-        
-        this.setState({array: row});
-        this.setState({rtop: 5});
-        this.setState({rleft: 5});
-
-    }
-    render(){
-        const {array} = this.state;
-        const {rtop} = this.state;
-        const {rleft} = this.state;
-        return(
-            
-            <div className = "d-flex">
-                <div className ="bg-light border-right" id = "sidebar-wrapper">
-                    <div class="sidebar-heading">Robot Cleaner</div>
-                    <div class="list-group list-group-flush">
-                        <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
-                        <a role = "button" onClick = {() => this.start()}  class="list-group-item list-group-item-action bg-light">Start</a>
-                        <a role = "button" onClick = {() => this.reset_array()}   class="list-group-item list-group-item-action bg-light">Reset</a>
-                    </div>
-                </div>
-                <div className = "bar-container">
-                    {array.map((value,index) => {
-                        return(
-                            <div className = "bar" key = {index} style = {{backgroundColor: value == 0 ? 'black' : 'rgb(106, 98, 145)' }}></div>
-                        );
-                    })}
-                    <div className = "robot" style = {{ top:`${rtop}px`, left: `${rleft}px` }}></div>
-                </div>
-                
-                        
-            </div>
-        )
-    }
+    this.setState({ array: row });
+    this.setState({ rtop: 5 });
+    this.setState({ rleft: 5 });
+  }
+  render() {
+    const { array } = this.state;
+    const { rtop } = this.state;
+    const { rleft } = this.state;
+    return (
+      <div className="d-flex">
+        <div
+          className="bg-dark border-right"
+          id="sidebar-wrapper"
+          style={{ height: "722px" }}
+        >
+          <div className="sidebar-heading">Robot Cleaner</div>
+          <div className="list-group list-group-flush">
+            <a
+              href="/"
+              className="list-group-item list-group-item-action bg-dark"
+              style={{ color: "inherit" }}
+            >
+              Reset
+            </a>
+            <a
+              role="button"
+              onClick={() => this.start()}
+              className="list-group-item list-group-item-action bg-dark"
+            >
+              Start
+            </a>
+          </div>
+        </div>
+        <div className="bar-container">
+          {array.map((value, index) => {
+            return (
+              <div
+                className="bar"
+                key={index}
+                style={{
+                  backgroundColor: value === 0 ? "black" : "rgb(106, 98, 145)",
+                }}
+              ></div>
+            );
+          })}
+          <div
+            className="robot"
+            style={{ top: `${rtop}px`, left: `${rleft}px` }}
+          ></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Main;
--- robotcleaner/src/components/robot.js
+++ robotcleaner/src/components/robot.js
@@ -1,92 +1,78 @@
-
-const directions = [[-1, 0],[0, 1],[1, 0],[0, -1]];
-var visited = new Set(); 
+const directions = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+];
+var visited = [];
 var animations = [];
 
-class robot {
-    constructor(array) {
-        this.array = array;
-        this.direction = 0; //  0: 'up', 1: 'right', 2: 'down', 3: 'left'
-        this.cell = [0,0];
-    }
-
-    turnRight(){
-        this.direction = 1;
-    }
-    turnLeft(){
-        this.direction = 3;
-    }
-    go_back(){
-        this.direction = 2;
-    }
-    move(){
-        if( this.cell[0] + directions[this.direction][0] < 0  
-            || this.cell[0] + directions[this.direction][0] >= 15 
-            || this.cell[1] + directions[this.direction][1] < 0 
-            || this.cell[1] + directions[this.direction][1] >= 20
-            || this.array[this.cell[0] + directions[this.direction][0]][this.cell[1]] === 0
-            || this.array[this.cell[0]][directions[this.direction][1] + this.cell[1]] === 0 
-        )
-                return false;
-        else{
-            this.cell[0] = this.cell[0] + directions[this.direction][0];
-            this.cell[1] = this.cell[1] + directions[this.direction][1];
-            return true;
-        }
-    }
-    clean(){
-        this.array[this.cell[0]][this.cell[1]] = 2;
-    }
-}; 
-
-export function robotclean(array) {
-        var ar = [];
-        
-        for(var i = 0;i<15;i++){
-            var c = [];
-            for(var j = 0; j<20;j++){
-                if(array[20*i+j] === 0)
-                    c.push(0);
-                else
-                    c.push(1);
-            }
-            ar.push(c);    
-        }
-        let robo = new robot(ar);
-
-        backtrack(robo);
-        return animations;
-    }
-
-function apush(array){
-    var c = [];
-    for(var i = 0;i<15;i++){
-        for(var j = 0; j<20;j++){
-            c.push(array[i][j]);
-        }
-    }
-    animations.push(c);
+function move(cell, array) {
+  if (cell[0] < 0 || cell[0] >= 15 || cell[1] < 0 || cell[1] >= 20)
+    return false;
+  else {
+    if (array[cell[0]][cell[1]] === 0) return false;
+    array[cell[0]][cell[1]] = 2;
+    return true;
+  }
 }
 
-function backtrack(robo){
-    console.log(robo,visited);
-    apush(robo.array);
-    visited.add(robo.cell);
-    robo.clean();
-    
-    for(var i = 0;i<4;i++){
+export function robotclean(arr) {
+  var ar = [];
 
-        var new_cell = [];
-        var new_d = (robo.direction + i) % 4
-        new_cell.push(robo.cell[0] + directions[new_d][0]); 
-        new_cell.push(robo.cell[1] + directions[new_d][1]);
-
-        if (visited.has(new_cell)==false && robo.move()){
-            backtrack(robo);
-            robo.go_back();
-        }
-       
-        robo.turnRight();
+  for (var i = 0; i < 15; i++) {
+    var c = [];
+    for (var j = 0; j < 20; j++) {
+      if (arr[20 * i + j] === 0) c.push(0);
+      else c.push(1);
     }
+    ar.push(c);
+  }
+  var direction = 0; //  0: 'up', 1: 'right', 2: 'down', 3: 'left'
+  var cell = [0, 0];
+  backtrack(ar, cell, direction);
+  //console.log(visited);
+  let finish = [];
+  finish.push(animations);
+  finish.push(visited);
+  return finish;
+}
 
+function apush(arr) {
+  var c = [];
+  for (var i = 0; i < 15; i++) {
+    for (var j = 0; j < 20; j++) {
+      c.push(arr[i][j]);
+    }
+  }
+  animations.push(c);
+}
+
+function has(array, item) {
+  for (var i = 0; i < array.length; i++) {
+    // This if statement depends on the format of your array
+    if (array[i][0] === item[0] && array[i][1] === item[1]) {
+      return true; // Found it
+    }
+  }
+  return false; // Not found
+}
+
+function backtrack(array, cell, direction) {
+  //console.log(visited);
+
+  apush(array, cell);
+  visited.push(cell);
+  array[cell[0]][cell[1]] = 2;
+
+  for (var i = 0; i < 4; i++) {
+    var new_cell = [];
+    var new_d = (direction + i) % 4;
+    new_cell.push(cell[0] + directions[new_d][0]);
+    new_cell.push(cell[1] + directions[new_d][1]);
+    //console.log(has(visited, new_cell));
+    if (has(visited, new_cell) === false && move(new_cell, array) === true) {
+      backtrack(array, new_cell, new_d);
+    }
+  }
 }
